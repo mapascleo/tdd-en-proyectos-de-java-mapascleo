92c8e7d0130231a84e9b7578af187a18eb8c1299
mapascleo <mapascleo@users.noreply.github.com>
2026-03-10 15:45:13 +0000

    deploy: 75145beab8c7c853fdf47a69a77deae54512cb8e

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.tt1.test"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.tt1.test"}];updateSearchResults();
